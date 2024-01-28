c3cd825137b8cfc4361f03fddf45840522c42708
unknown <abasian.arman@gmail.com>
2024-01-28 17:32:07 +0330

    refactor the style in PaymentListTable component

--- frontend/src/app/(admin)/admin/payments/PaymentListTable.jsx
+++ frontend/src/app/(admin)/admin/payments/PaymentListTable.jsx
@@ -13,7 +13,7 @@ function PaymentListTable({ payments }) {
             {adminPaymentListTableTHeads.map((item) => {
               return (
                 <th className="whitespace-nowrap table__th" key={item.id}>
-                  {item.label}
+                 <p className="tdItem--table">{item.label}</p> 
                 </th>
               );
             })}
@@ -23,14 +23,14 @@ function PaymentListTable({ payments }) {
           {payments.map((payment, index) => {
             return (
               <tr key={payment._id}>
-                <td className="table__td">{index + 1}</td>
+                <td className="table__td"><p className="tdItem--table">{index + 1}</p></td>
                 <td className="table__td  whitespace-nowrap truncate">
-                  {payment.invoiceNumber}
+                <p className="tdItem--table">{payment.invoiceNumber}</p>
                 </td>
                 <td className="table__td  max-w-[280px] whitespace-nowrap truncate">
-                  {payment.description}
+                <p className="tdItem--table">{payment.description}</p>
                 </td>
-                <td className="table__td  whitespace-nowrap truncate">
+                <td className="table__td  whitespace-nowrap truncate tdItem--table">
                   <div className="flex flex-col gap-y-2">
                     <span> {payment.user.name}</span>
                     <span> {payment.user.email}</span>
@@ -40,7 +40,7 @@ function PaymentListTable({ payments }) {
                   </div>
                 </td>
                 <td className="table__td">
-                  <div className="flex flex-col gap-y-2 items-start">
+                  <div className="flex flex-col gap-y-2 items-start tdItem--table">
                     {payment.cart.productDetail.map((product) => {
                       return (
                         <span
@@ -54,23 +54,23 @@ function PaymentListTable({ payments }) {
                   </div>
                 </td>
                 <td className="table__td font-bold text-lg">
-                  {toNumbersWithComma(payment.amount)}
+                <p className="tdItem--table">{toNumbersWithComma(payment.amount)}</p> 
                 </td>
                 <td className="table__td">
-                  {toLocalDateStringShort(payment.createdAt)}
+                <p className="tdItem--table">{toLocalDateStringShort(payment.createdAt)}</p> 
                 </td>
                 <td className="table__td">
                   {payment.status === "COMPLETED" ? (
-                    <span className="badge badge--success">successful</span>
+                    <span className="badge badge--success tdItem--table">successful</span>
                   ) : (
-                    <span className="badge badge--error">unsuccessful</span>
+                    <span className="badge badge--error tdItem--table">unsuccessful</span>
                   )}
                 </td>
                 <td>
                   <div>
                     <Link
                       href={`/admin/payments/${payment._id}`}
-                      className="flex justify-center"
+                      className="flex justify-center tdItem--table"
                     >
                       <HiEye className="w-6 h-6 text-primary-900" />
                     </Link>
